f105e5f786748b78aab7495e5a653dcd41ffb353
CurryDevs <currydevs.co@gmail.com>
2025-09-14 11:06:38 +0530

    remove smooth scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SmoothScrolling from "@/hooks/smooth-scrolling";
+// import SmoothScrolling from "@/hooks/smooth-scrolling";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -13,7 +13,7 @@ const App = () => {
   // Initialize smooth scrolling for the entire app
 
   return (
-    <SmoothScrolling>
+    // <SmoothScrolling>
       <QueryClientProvider client={queryClient}>
         <TooltipProvider>
           <Toaster />
@@ -27,7 +27,7 @@ const App = () => {
           </BrowserRouter>
         </TooltipProvider>
       </QueryClientProvider>
-    </SmoothScrolling>
+    // </SmoothScrolling>
   );
 };
 
