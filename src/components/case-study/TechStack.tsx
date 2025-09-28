6fcd17928f67ecfb017611baed7d847526e66fd5
24-Gautam <gautam2412003@gmail.com>
2025-09-29 00:08:45 +0530

    Added 4th Project JSON Data

--- src/components/case-study/TechStack.tsx
+++ src/components/case-study/TechStack.tsx
@@ -1,6 +1,6 @@
 import {
   SiReact, SiTypescript, SiTailwindcss, SiFramer, SiExpo, SiExpress, SiPostgresql, SiRedis,
-  SiKubernetes, SiAmazonwebservices, SiGoogleanalytics, SiMixpanel, SiHotjar, SiAuth0, SiJsonwebtokens, SiSst, SiMdx, SiJavascript, SiHtml5, SiCss3, SiAstro, SiVercel
+  SiKubernetes, SiAmazonwebservices, SiGoogleanalytics, SiMixpanel, SiHotjar, SiAuth0, SiJsonwebtokens, SiSst, SiMdx, SiJavascript, SiHtml5, SiCss3, SiAstro, SiVercel, SiNextdotjs
 } from "react-icons/si";
 import { FaNodeJs, FaAws, FaDocker, FaFigma } from "react-icons/fa";
 import { MdInstallMobile, MdSpeed } from "react-icons/md";
@@ -9,7 +9,7 @@ import { MdInstallMobile, MdSpeed } from "react-icons/md";
 const iconMap = {
   SiReact, SiTypescript, SiTailwindcss, SiFramer, SiExpo, FaNodeJs, SiExpress, SiPostgresql, SiRedis,
   FaAws, FaDocker, SiKubernetes, SiAmazonwebservices, FaFigma, SiGoogleanalytics, SiMixpanel, SiHotjar,
-  SiAuth0, SiJsonwebtokens, SiSst, MdInstallMobile, MdSpeed, SiMdx, SiJavascript, SiHtml5, SiCss3, SiAstro, SiVercel
+  SiAuth0, SiJsonwebtokens, SiSst, MdInstallMobile, MdSpeed, SiMdx, SiJavascript, SiHtml5, SiCss3, SiAstro, SiVercel, SiNextdotjs
 };
 
 const TechStack = ({ techStack }) => {
